1080d1ec234aebd24c5171064626ddb7b9a36460
Nimish kamthe <nimish.kamthe710@gmail.com>
2026-08-20 01:45:55 +0530

    Time: 9 ms (25.28%), Space: 46.3 MB (87.76%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,33 +1,28 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb= new StringBuilder(s);
-        int start=0;
-        for(int i=0;i<sb.length();i++){
-            if(sb.charAt(i)==' '){
-                int end= i-1;
-                while(start<=end){
-                    char temp= sb.charAt(start);
-                    sb.setCharAt(start,sb.charAt(end));
-                    sb.setCharAt(end,temp);
+        StringBuilder sb = new StringBuilder(s);
+
+        int start = 0;
+
+        for (int i = 0; i <= sb.length(); i++) {
+
+            if (i == sb.length() || sb.charAt(i) == ' ') {
+
+                int end = i - 1;
+
+                while (start < end) {
+                    char temp = sb.charAt(start);
+                    sb.setCharAt(start, sb.charAt(end));
+                    sb.setCharAt(end, temp);
+
                     start++;
                     end--;
                 }
-                start= i+1;
-            
+
+                start = i + 1;
             }
-            
         }
-        int end = sb.length() - 1;
-        while(start <= end) {
-            char temp = sb.charAt(start);
-            sb.setCharAt(start, sb.charAt(end));
-            sb.setCharAt(end, temp);
-            start++;
-            end--;
-        }
-            
-        String str= sb.toString();
-        return str;
-        
+
+        return sb.toString();
     }
-}   
+}
